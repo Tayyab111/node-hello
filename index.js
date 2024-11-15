6315b0129249d9ea9ee097935f12d187cc90e308
Tayyab111 <tayyabafridi843@gmail.com>
2024-11-15 22:29:33 +0500

    now

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const http = require('http');
+\const http = require('http');
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node...done bacha, increase auto working...\n'
+  const msg = 'Hello Node...done bacha, added cleanup...\n'
   res.end(msg);
 });
 
