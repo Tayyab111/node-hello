15f2126524d3e8b90e873f6c867ff0e058e5f059
Tayyab111 <tayyabafridi843@gmail.com>
2024-11-15 21:55:26 +0500

    exe

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node...done bacha, ok I think it will work...\n'
+  const msg = 'Hello Node...done bacha, increase executers...\n'
   res.end(msg);
 });
 
