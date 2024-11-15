6d450046b164a6100a064acd16b888ff94db92e4
Tayyab111 <tayyabafridi843@gmail.com>
2024-11-15 21:41:32 +0500

    ko

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node...done bacha, webhoot is now really working...\n'
+  const msg = 'Hello Node...done bacha, jojo...\n'
   res.end(msg);
 });
 
