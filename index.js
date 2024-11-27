38ad2bb4ab896c5ce3d722fa34ea04a3992aa4e4
Tayyab111 <tayyabafridi843@gmail.com>
2024-11-27 22:26:08 +0500

    ok

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node...done bacha, checking.ok.bro.\n'
+  const msg = 'Hello Node...done bacha, checking.ok.bro1.\n'
   res.end(msg);
 });
 
