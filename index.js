e69d69eeeb79fb2c1fa395205d16d49792892e1b
Tayyab111 <tayyabafridi843@gmail.com>
2024-11-30 16:35:36 +0500

    this commit for git hub web-hook...

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node...done bacha, Jenkins latest+latest.\n'
+  const msg = 'Hello Node...done bacha, Jenkins after web-hook error...\n'
   res.end(msg);
 });
 
